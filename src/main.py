8a9da0dd655b0b4981b2d649e263c8b928175f99
shimizuyuta213@gmail.com <shimizuyuta213@gmail.com>
2023-05-15 10:04:10 +0900

    update: リファクタリング

--- src/main.py
+++ src/main.py
@@ -1,56 +1,63 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.alert import Alert
 import time
-import os 
+import os
 
-def _main():
+
+def create_webdriver():
     options = webdriver.ChromeOptions()
     options.add_argument('--no-sandbox')
-    
-    driver = webdriver.Remote(
+    return webdriver.Remote(
         command_executor='http://selenium:4444/wd/hub',
         options=options
     )
+
+
+def login(driver, email, password):
+    driver.get('https://app.microcms.io/signin')
+    driver.find_element(By.NAME, 'email').send_keys(email)
+    driver.find_element(By.NAME, 'password').send_keys(password)
+    driver.find_element(By.XPATH, '//*[@id="root"]/div[1]/div/div/div/div/div/div[2]/div[1]/button').click()
+
+
+def create_new_blog_post(driver):
+    driver.find_element(By.XPATH, '//*[@id="root"]/div[1]/div/div/div/div/div/table/tbody/tr').click()
+    time.sleep(3)
+    driver.find_element(By.XPATH, '//*[@id="root"]/div[1]/div[2]/div/main/div/div/div[1]/div[2]/a').click()
+    time.sleep(2)
+    driver.find_element(By.TAG_NAME, 'input').send_keys('test title')
+    driver.find_element(By.CLASS_NAME, 'ql-editor').send_keys('test contents')
+    time.sleep(1)
+    driver.find_element(By.XPATH, '//*[@id="root"]/div[1]/div[2]/div/main/div/div/div[1]/div[2]/button[2]').click()
+    time.sleep(1)
+    Alert(driver).accept()
+
+
+def main():
+    driver = None
     try:
-        driver.get('https://app.microcms.io/signin')
-        # ユーザー名とパスワードの入力フィールドを見つける
-        email_field = driver.find_element(By.NAME, 'email')  
-        password_field = driver.find_element(By.NAME, 'password')  
-        # ユーザー名とパスワードを入力
-        email = os.environ.get('EMAIL')
-        password = os.environ.get('PASSWORD')
-        email_field.send_keys(email)
-        password_field.send_keys(password)
-        time.sleep(1)
-        # ログインボタンをクリック
-        login_button = driver.find_element(By.XPATH, '//*[@id="root"]/div[1]/div/div/div/div/div/div[2]/div[1]/button')
-        login_button.click()
+        driver = create_webdriver()
+        login(driver, os.getenv('EMAIL'), os.getenv('PASSWORD'))
         time.sleep(3)
-        # ブログをクリック
-        element = driver.find_element(By.XPATH, '//*[@id="root"]/div[1]/div/div/div/div/div/table/tbody/tr')
-        element.click()
-        time.sleep(3)
-        # 新規作成をクリック
-        link = driver.find_element(By.XPATH,'//*[@id="root"]/div[1]/div[2]/div/main/div/div/div[1]/div[2]/a')
-        link.click()
-        time.sleep(2)
-        # タイトルと本文を入力
-        title = driver.find_element(By.TAG_NAME,'input')
-        contents = driver.find_element(By.CLASS_NAME,'ql-editor')
-        title.send_keys('test title')
-        contents.send_keys('test contents')
-        time.sleep(1)
-        # 保存をクリック
-        openBlog = driver.find_element(By.XPATH,'//*[@id="root"]/div[1]/div[2]/div/main/div/div/div[1]/div[2]/button[2]')
-        openBlog.click()
-        time.sleep(1)
-        # ブログを確認をクリック
-        Alert(driver).accept()
+        create_new_blog_post(driver)
         print('ブログ記事を投稿しました。')
+    except NoSuchElementException as e:
+        print('エラー：', e)
+        if driver:
+            print('エラーが発生したURL：', driver.current_url)
+            driver.save_screenshot('error.png')
     except Exception as e:
-        print('エラー：',e)
+        print('未知のエラー：', e)
+        if driver:
+            print('エラーが発生したURL：', driver.current_url)
+            driver.save_screenshot('error.png')
     finally:
-        driver.quit()
+        if driver:
+            driver.quit()
+
+
 if __name__ == '__main__':
-    _main()
+    main()
